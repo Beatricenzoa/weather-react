db417e7a48f97920563b2076b1caa11007c098aa
Beatricenzoa <beatricenzoa@gmail.com>
2024-08-01 14:31:37 +0300

    imported axios and modified my search engine to fetch weather details from the weather API

--- src/Search.js
+++ src/Search.js
@@ -1,18 +1,37 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 export default function Search(props) {
   let [city, setCity] = useState("");
+  let [weather, setWeather] = useState({});
+  let [searched, setSearched] = useState(false);
+
+  function displayWeather(response) {
+    setSearched(true);
+    setWeather({
+      temperature: Math.round(response.data.temperature.current),
+      humidity: response.data.temperature.humidity,
+      icon: response.data.condition.icon_url,
+      description: response.data.condition.description,
+      wind: response.data.wind.speed,
+    });
+    console.log(response.data);
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
+    const key = "0f8t20affd39ebb0d8d3f0o83cf0b40f";
+    const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}&units=metric`;
+
+    axios.get(apiUrl).then(displayWeather);
   }
 
   function handleChange(event) {
     setCity(event.target.value);
   }
 
-  let header = (
+  const header = (
     <form id="search-form" onSubmit={handleSubmit}>
       <input
         type="search"
@@ -24,35 +43,61 @@ export default function Search(props) {
       <input type="submit" value="search" id="search-button" />
     </form>
   );
-  let main = (
+  const main = (
     <main>
-      <div class="current-weather">
-        <div class="current-details">
-          <h1 id="current-city">{city}</h1>
-          <p>
-            <span id="current-date"></span>,<span id="description"></span>
-          </p>
+      <h1 id="current-city">{city}</h1>
+      <div className="current-details">
+        <div className="current-weather">
+          <div className="precipitation">
+            <p>{weather.description}</p>
+            <p>
+              Humidity: <strong id="humidity">{weather.humidity}%</strong>
+              <br />
+              Wind: <strong id="wind">{weather.wind}km/h</strong>
+            </p>
+          </div>
         </div>
-        <div class="precipitation">
-          <p>
-            Humidity: <strong id="humidity"></strong>
-            <br />
-            Wind: <strong id="wind"></strong>
-          </p>
+        <div id="temperature-details">
+          <div id="icon">
+            <img src={weather.icon} alt="weather icon" />
+          </div>
+          <div id="temperature">{weather.temperature}</div>
+          <div id="unit">℃</div>
         </div>
       </div>
-      <div id="temperature-details">
-        <div id="icon"></div>
-        <div id="temperature"></div>
-        <div id="unit">℃</div>
-      </div>
-      <div id="weather-forecast"></div>
     </main>
   );
-  return (
-    <div>
-      {header}
-      {main}
-    </div>
+  const footer = (
+    <p>
+      This project was coded by{" "}
+      <a href="https://github.com/Beatricenzoa" target="_blank">
+        Beatrice Mutuku
+      </a>{" "}
+      and is{" "}
+      <a href="https://github.com/Beatricenzoa/weather-react" target="_blank">
+        on GitHub
+      </a>{" "}
+      and hosted on{" "}
+      <a href="#" target="_blank">
+        {" "}
+        Netlify
+      </a>
+    </p>
   );
+  if (searched === true) {
+    return (
+      <div>
+        {header}
+        {main}
+        {footer}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {header}
+        {footer}
+      </div>
+    );
+  }
 }
