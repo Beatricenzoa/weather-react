f5f35ba140cb7e3aa71b52dd567b3f270b67a0a0
Beatricenzoa <beatricenzoa@gmail.com>
2024-08-02 16:21:33 +0300

    Added date in the search component

--- src/Search.js
+++ src/Search.js
@@ -1,21 +1,22 @@
 import React, { useState } from "react";
+import CurrentDate from "./CurrentDate";
 import axios from "axios";
 import "./App.css";
 
 export default function Search(props) {
-  let [city, setCity] = useState(props.defaultCity);
-  let [weather, setWeather] = useState({});
-  let [searched, setSearched] = useState(false);
+  const [city, setCity] = useState(props.defaultCity);
+  const [weather, setWeather] = useState({ searched: false });
 
   function displayWeather(response) {
-    setSearched(true);
     setWeather({
+      searched: true,
       temperature: Math.round(response.data.temperature.current),
       humidity: response.data.temperature.humidity,
       icon: response.data.condition.icon_url,
       description: response.data.condition.description,
       wind: response.data.wind.speed,
       city: response.data.city,
+      date: new Date(response.data.time * 1000),
     });
   }
 
@@ -23,14 +24,16 @@ export default function Search(props) {
     event.preventDefault();
     searchWeather();
   }
+
+  function handleChange(event) {
+    setCity(event.target.value);
+  }
+
   function searchWeather() {
     const key = "0f8t20affd39ebb0d8d3f0o83cf0b40f";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}&units=metric`;
 
     axios.get(apiUrl).then(displayWeather);
-  }
-  function handleChange(event) {
-    setCity(event.target.value);
   }
 
   const header = (
@@ -45,16 +48,16 @@ export default function Search(props) {
       <input type="submit" value="search" id="search-button" />
     </form>
   );
-  const currentWeather = (
+  let currentWeather = (
     <main>
       <h1 id="current-city">{weather.city}</h1>
       <div className="current-details">
         <div className="current-weather">
           <div className="precipitation">
-            <p>{weather.description}</p>
+            <CurrentDate date={weather.date} />
+            <p> {weather.description}</p>
             <p>
-              Humidity: <strong id="humidity">{weather.humidity}%</strong>
-              <br />
+              Humidity: <strong id="humidity">{weather.humidity}%</strong>,
               Wind: <strong id="wind">{weather.wind}km/h</strong>
             </p>
           </div>
@@ -67,43 +70,42 @@ export default function Search(props) {
           <div id="unit">℃</div>
         </div>
       </div>
+      <footer>
+        <p>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/Beatricenzoa"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Beatrice Mutuku
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Beatricenzoa/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            on GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://jazzy-marshmallow-fc8a82.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Netlify
+          </a>
+        </p>
+      </footer>
     </main>
   );
-  const footer = (
-    <p>
-      This project was coded by{" "}
-      <a
-        href="https://github.com/Beatricenzoa"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Beatrice Mutuku
-      </a>{" "}
-      and is{" "}
-      <a
-        href="https://github.com/Beatricenzoa/weather-react"
-        target="_blank"
-        rel="noreferrer"
-      >
-        on GitHub
-      </a>{" "}
-      and hosted on{" "}
-      <a
-        href="https://jazzy-marshmallow-fc8a82.netlify.app/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        {" "}
-        Netlify
-      </a>
-    </p>
-  );
-  if (searched === true) {
+  if (weather.searched === true) {
     return (
       <div>
         {header}
         {currentWeather}
-        {footer}
       </div>
     );
   } else {
@@ -111,7 +113,6 @@ export default function Search(props) {
       <div>
         {header}
         {searchWeather()}
-        {footer}
       </div>
     );
   }
